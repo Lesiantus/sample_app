6f9afb992c9de326951cec3363de62b61c14c8a7
Lesiantus <sashkabonan@gmail.com>
2020-11-02 20:57:56 +0700

    Plus heroku username and password hardcoded2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # config.require_master_key = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://blooming-badlands-07906.herokuapp.com'
+  host = 'blooming-badlands-07906.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
