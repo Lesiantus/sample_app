c7577944bf427ccb416e17c3e91e0f41fe39208e
Lesiantus <sashkabonan@gmail.com>
2020-11-02 19:52:57 +0700

    Plus heroku username and password hardcoded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['app182967145@heroku.com'],
+    :password => ENV['qsi8frd86141'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
